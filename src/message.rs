1a823a8626a0313d16d93f21d0dbc1d9c15a4862
Mohammad Samir <msamirdev92@gmail.com>
2017-06-16 07:12:42 +0300

    reserve capacity on BytesMut before calling put

--- src/message.rs
+++ src/message.rs
@@ -64,8 +64,12 @@ impl Encoder for NsqResponder {
 
     fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
         match item {
-            MessageReply::Nop => dst.put("NOP\n"),
+            MessageReply::Nop => {
+                dst.reserve(4);
+		dst.put("NOP\n");
+            }
             MessageReply::Fin(id) => {
+                dst.reserve(21);
                 dst.put("FIN ");
                 dst.put(id);
                 dst.put("\n");
